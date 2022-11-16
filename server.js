8c7410badab92e74c6b83baa95ade2de546635d8
Vembarasan Nagarajan <vembarasugn@gmil.com>
2022-11-16 19:20:43 +0530

    Server file updated

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); 
-const middlewares = jsonServer.defaults();
-const port = 3000; 
 
-server.use(middlewares);
-server.use(router);
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-server.listen(port);
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running at 3000.!')
+})
